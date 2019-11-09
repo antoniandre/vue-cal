1e2bf13abd4a8c77a46af1cd99957089cc991f04
twoody <tanner.l.woody@gmail.com>
2019-11-09 15:46:30 -0800

    Reverted back to master for no merge conflicts

--- src/views/isolated-test-view.vue
+++ src/views/isolated-test-view.vue
@@ -3,77 +3,34 @@
 vue-cal(
   :selected-date="selectedDate"
   :events="events"
-  :hide-weekdays="[1,2,3,4,5]"
   editable-events
+  twelve-hour
   style="min-height: 400px;max-height: 65vh")
-  template(v-slot:weekday-renderer="{ heading, view }")
-    table.myheaders(
-      v-if="view.view.id === 'week'")
-      tr.headingrow
-        td
-          span.dateCircle {{ (heading.heading.dayOfMonth) }}
-        td.headingcell
-          span.headingmonth {{ getMonth(heading.heading.date) }}
-          br
-          span.headingday {{ heading.heading.full }}
 </template>
 
 <script>
 import VueCal from '@/components/vue-cal'
 
+const now = new Date()
 export default {
   components: { VueCal },
   data: () => ({
-    selectedDate: '2020-04-05',
+    selectedDate: now,
     events: [
-      // {
-      //   startDate: new Date(new Date(now).setHours(9, 0, 0, 0)),
-      //   endDate: new Date(new Date(now).setHours(12, 0, 0, 0)),
-      //   title: 'Event'
-      // },
       {
-        start: '2020-04-04 00:00',
-        end: '2020-04-05 08:00',
+        startDate: new Date(new Date(now).setHours(9, 0, 0, 0)),
+        endDate: new Date(new Date(now).setHours(12, 0, 0, 0)),
         title: 'Event'
       }
     ]
   }),
 
   methods: {
-    getMonth (datestring) {
-      return datestring.toString().split(' ')[1]
-    },
-    bar (heading) {
-      return heading.heading.full
-    }
   }
 }
 </script>
 
 <style lang="scss">
-
-  .dateCircle {
-    border: 1px solid black;
-    border-radius: 30px;
-    text-align:right;
-    margin-right:10px;
-    padding-right: 5px;
-    padding-left: 5px;
-  }
-  .myheaders {
-  }
-  .headingrow {
-  }
-  .headingcell {
-    text-align:left;
-  }
-  .headingmonth {
-    text-align: left;
-  }
-  .headingday {
-    text-align: left;
-  }
-
-  .v-application--wrap {min-height: 0;}
-  footer {display: none !important;}
+.v-application--wrap {min-height: 0;}
+footer {display: none !important;}
 </style>
