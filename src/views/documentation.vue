e632d60ed21ebe6686ee72f2f4938664d4edc823
Antoni André <antoniandre.web@gmail.com>
2020-03-11 23:50:17 +1100

    Update documentation.

--- src/views/documentation.vue
+++ src/views/documentation.vue
@@ -1130,9 +1130,17 @@
   p.mb-2.
     You can drag &amp; drop events from an external source as long as they are HTML5 draggable (this will change when touch devices are supported).#[br]
     It is also possible to move an event from one calendar to another.#[br]#[br]
-    In your external event, you can set a #[span.code duration] property: it will be used to represent the duration of the event on Vue Cal when it has no date.#[br]
+    In the external event, you can set a #[span.code duration] property: it will be used to represent the duration of the event on Vue Cal when it has no date.#[br]
     If the #[span.code duration] is missing, the default will be 2 hours.
 
+  highlight-message(type="tips")
+    strong Important note when dragging external events into Vue Cal:
+    div.
+      With HTML5 drag &amp; drop, when you drop a DOM element to another location, you have to move
+      the element yourself. Now especially because Vue is data driven and a DOM update does not
+      modify the data, you will also have to remove the event from its original data source yourself
+      - unless you want to create a copy.#[br]
+      Learn how in the example source code bellow.
   v-layout.mt-4(wrap)
     div.mr-2
       .external-event(
@@ -1146,22 +1154,23 @@
       small
       hide-view-selector
       hide-weekends
-      :disable-views=['years', 'year', 'month', 'day']
+      :disable-views="['years', 'year', 'month', 'day']"
       :time-from="9 * 60"
       :time-to="16 * 60"
       editable-events
-      @event-drop="onEventDrop($event)")
+      @event-drop="onEventDrop")
     vue-cal.ml-1.flex.external-events-drag-and-drop.vuecal--green-theme(
       small
       hide-view-selector
       hide-weekends
-      :disable-views=['years', 'year', 'month', 'day']
+      :disable-views="['years', 'year', 'month', 'day']"
       :time-from="9 * 60"
       :time-to="16 * 60"
       editable-events
-      @event-drop="onEventDrop($event)")
+      @event-drop="onEventDrop")
 
   sshpre(language="html-vue" label="Vue Template").
+    &lt;!-- Three HTML5 draggable events. --&gt;
     &lt;div class="external-event"
          v-for="(item, i) in draggables"
          :key="i"
@@ -1170,23 +1179,39 @@
          &lt;strong&gt;{{ '\{\{ item.title \}\}' }}&lt;/strong&gt;
          ({{ "\{\{ item.duration ? `${item.duration} min` : 'no duration' \}\}" }})
     &lt;/div&gt;
+
+    &lt;vue-cal small
+             hide-view-selector
+             hide-weekends
+             :disable-views="['years', 'year', 'month', 'day']"
+             :time-from="9 * 60"
+             :time-to="16 * 60"
+             editable-events
+             @event-drop="onEventDrop"&gt;
+    &lt;/vue-cal&gt;
   sshpre(language="js" label="Javascript - Vue Component").
     export default {
       data: () => ({
         draggables: [
           {
+            // The id (or however you name it), will help you find which event to delete
+            // from the callback triggered on drop into Vue Cal.
+            id: 1,
             title: 'Ext. Event 1',
             content: 'content 1',
             duration: 60
           },
           {
+            id: 2,
             title: 'Ext. Event 2',
             content: 'content 2',
             duration: 30
           },
           {
+            id: 3,
             title: 'Ext. Event 3',
             content: 'content 3'
+            // No defined duration here: will default to 2 hours.
           }
         ]
       }),
@@ -1194,6 +1219,19 @@
         onEventDragStart (e, draggable) {
           // Passing the event's data to Vue Cal through the DataTransfer object.
           e.dataTransfer.setData('event', JSON.stringify(draggable))
+        },
+        // The 3 parameters are destructured from the passed $event in @event-drop="onEventDrop".
+        // `event` is the final event as Vue Cal understands it.
+        // `originalEvent` is the event that was dragged into Vue Cal, it can come from the same
+        //  Vue Cal instance, another one, or an external source.
+        // `external` is a boolean that lets you know if the event is not coming from any Vue Cal.
+        onEventDrop ({ event, originalEvent, external }) {
+          // If the event is external, delete it from the data source on drop into Vue Cal.
+          // If the event comes from another Vue Cal instance, it will be deleted automatically in there.
+          if (external) {
+            const extEventToDeletePos = this.draggables.findIndex(item => item.id === originalEvent.id)
+            if (extEventToDeletePos > -1) this.draggables.splice(extEventToDeletePos, 1)
+          }
         }
       }
     }
