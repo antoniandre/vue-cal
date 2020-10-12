55f182a5891125985f18d0f35655e06a098844ab
Antoni Andre <antoniandre.web@gmail.com>
2020-10-13 10:48:12 +1100

    Update eslint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,8 @@ module.exports = {
   rules: {
     'brace-style': [2, 'stroustrup', { allowSingleLine: true }],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/no-mutating-props': 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
