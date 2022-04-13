b0b818c407b2b4af2befa8d23ac0c6187fa280b1
antoni andre <antoniandre.web@gmail.com>
2022-04-12 22:41:40 -0400

    Add Wave UI. :metal:

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
-import { createApp, h } from 'vue'
+import { createApp } from 'vue'
 import router from './router'
+import WaveUI from 'wave-ui'
+import 'wave-ui/dist/wave-ui.css'
 import App from './app.vue'
 
-createApp({
-  render: () => h(App)
-}).use(router).mount('#app')
+import '@fortawesome/fontawesome-free/css/fontawesome.css'
+import '@fortawesome/fontawesome-free/css/brands.css'
+
+const app = createApp(App).use(router)
+
+new WaveUI(app, {
+  iconsLigature: 'material-icons',
+  colors: {
+    primary: '#42b983',
+    secondary: '#2c3e50',
+    lightgrey: '#eee'
+  }
+})
+
+app.mount('#app')
