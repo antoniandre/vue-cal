b6add2206f294b07d51787830a728c435f0ca5e3
antoniandre <antoniandre.web@gmail.com>
2024-04-12 17:27:26 +0200

    Separate the main calendar's body in its own file.

---
+++ src/vue-cal/components/body.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+.vue-cal__body
+  .vue-cal__cell(v-for="i in 42" :index="i") {{ i }}
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss">
+.vue-cal__body {
+  display: grid;
+  grid-template-columns: 6;
+  grid-template-rows: 7;
+  height: 100%;
+}
+</style>
