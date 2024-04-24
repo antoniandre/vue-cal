1009ae89ec2426f05b302732a1846ccbb01e6be4
antoniandre <antoniandre.web@gmail.com>
2024-04-24 18:25:45 +0200

    Update the Vite config for new deps versions.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { terser } from "rollup-plugin-terser" // Minifier.
 import { resolve } from 'path'
 import pkg from './package.json'
 import autoprefixer from 'autoprefixer'
@@ -14,6 +13,7 @@ const banner = `/**
   */\n`
 
 const bundlingConf = {
+  minify: true,
   lib: {
     entry: resolve(__dirname, 'src/vue-cal/index.vue'),
     name: 'vuecal' // For IIFE.
@@ -30,42 +30,10 @@ const bundlingConf = {
         entryFileNames: 'vue-cal.[format].js',
         chunkFileNames: '[name].js',
         banner,
-        plugins: [terser()], // Minify.
         manualChunks(id) {
           const match = /i18n\/(.{2,5})\.json/.exec(id)
           if (match) return 'i18n/' + match[1] + '.es'
           else if (id.includes('drag-and-drop')) return 'drag-and-drop.es'
-        }
-      },
-
-      // Works with dynamic import().
-      {
-        format: 'cjs',
-        dir: 'dist',
-        entryFileNames: 'vue-cal.[format].js',
-        chunkFileNames: '[name].js',
-        banner,
-        manualChunks(id) {
-          const match = /i18n\/(.{2,5})\.json/.exec(id)
-          if (match) return 'i18n/' + match[1] + '.cjs'
-          else if (id.includes('drag-and-drop')) return 'drag-and-drop.cjs'
-        }
-      },
-
-      // Works with dynamic import().
-      {
-        format: 'amd',
-        name: 'vuecal',
-        dir: 'dist',
-        entryFileNames: 'vue-cal.[format].js',
-        chunkFileNames: '[name].js',
-        // Provide global variables to use in the UMD build for externalized deps.
-        globals: { vue: 'Vue' },
-        banner,
-        manualChunks(id) {
-          const match = /i18n\/(.{2,5})\.json/.exec(id)
-          if (match) return 'i18n/' + match[1] + '.amd'
-          else if (id.includes('drag-and-drop')) return 'drag-and-drop.amd'
         }
       },
 
@@ -83,8 +51,6 @@ const bundlingConf = {
     ]
   }
 }
-
-const build = process.env.BUNDLE ? bundlingConf : { outDir: 'docs' }
 
 export default defineConfig({
   define: {
@@ -117,5 +83,5 @@ export default defineConfig({
       plugins: [autoprefixer]
     }
   },
-  build
+  build: process.env.BUNDLE ? bundlingConf : { outDir: 'docs' }
 })
