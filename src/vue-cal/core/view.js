ba223476479b21b5ab6a0c16ed960a3d42ff442b
antoniandre <antoniandre.web@gmail.com>
2024-07-08 08:38:39 +0200

    Cleanup.

--- src/vue-cal/core/view.js
+++ src/vue-cal/core/view.js
@@ -278,11 +278,11 @@ export const useView = vuecal => {
     // recompute all the cells! But if forced (forceUpdate), just do it.
     // Before checking if the date is in view range, use the firstCellDate and lastCellDate unless on month view
     // (where we still want to navigate when clicking a cell that is out of range).
-    let [start, end] = [firstCellDate.value, lastCellDate.value]
-    if (viewId.value === 'month') ([start, end] = [start.value, end.value])
-    if (!dateUtils.isInRange(date, start, end) || forceUpdate) {
+    let [viewStart, viewEnd] = [firstCellDate.value, lastCellDate.value]
+    if (viewId.value === 'month') ([viewStart, viewEnd] = [start.value, end.value])
+    if (!dateUtils.isInRange(date, viewStart, viewEnd) || forceUpdate) {
       date.setHours(0, 0, 0, 0)
-      transitionDirection.value = date.getTime() < start.getTime() ? 'left' : 'right'
+      transitionDirection.value = date.getTime() < viewStart.getTime() ? 'left' : 'right'
       viewDate.value = date
       if (emitUpdate) emit('update:viewDate', date)
       updateView()
@@ -378,9 +378,9 @@ export const useView = vuecal => {
     cellDates,
     cols,
     rows,
-    // All the events are stored in the mutableEvents array, but subset of visible ones are passed
-    // Into the current view for fast lookup and manipulation.
-    events,
+    // All the events are stored and indexed by dates in the config.eventsByDates array.
+    // The following events array is only a subset of visible ones.
+    // events,
     switch: switchView,
     previous,
     next,
