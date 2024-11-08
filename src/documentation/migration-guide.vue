ee1f9a72620e90e68f43eb03066d77eea9a2188f
antoniandre <antoniandre.web@gmail.com>
2024-11-08 17:04:06 +0100

    Doc: Update the migration guide.

--- src/documentation/migration-guide.vue
+++ src/documentation/migration-guide.vue
@@ -9,15 +9,23 @@ ul
       import { addDatePrototypes } from '@/vue-cal'
 
       addDatePrototypes()
-  li No more header right margin to align with body if there is a scrollbar.
+  li.
+    No more header right margin to align with body if there is a scrollbar. Now the Vue Cal body is
+    scrollable with sticky headers (weekdays and time column) in both axis.
   li.
     The selected date and view date are now 2 separated variables: #[code selectedDate] and
     #[code viewDate], so they can be updated distinctly without the side effect of the other.
   li.
     You can now directly attach any valid DOM event you want to the events and cells.
     It should only start respectively with #[code event-] or #[code cell-] to be forwarded.
+  li.
+    Navigation by clicking or double-clicking cell has been removed, you can add this yourself
+    now that you can directly attach any valid DOM event to the cell.
+  li.
+    The default theme is used by default. No need to add the class anymore, but you can disable and
+    style from scratch by setting the #[code theme] prop to false or a different class string.
 
-h2 Props
+h2.mt12 Props
 ul
   li `activeView` renamed `view`
   li removed `hideBody` useless. With so much flexibility, there is no case where you only need the header without the calendar body
@@ -29,6 +37,8 @@ ul
   li disableWeekdays -> days indexes should now be provided as 3 letter strings like 'mon', 'tue', 'wed', etc.
   li hideViewsBar renamed viewsBar, default false
   li todayButton now defaults to true, previously false
+  li clickToNavigate -> removed, you can attach your own DOM events to cells.
+  li dblClickToNavigate -> removed, you can attach your own DOM events to cells.
 </template>
 
 <script setup>
