35a5da9e53d61b0713b70ee2b4d297cddab7e066
antoniandre <antoniandre.web@gmail.com>
2024-11-13 00:05:09 +0100

    Doc: Update the date and time examples.

--- src/documentation/examples/date-and-time.vue
+++ src/documentation/examples/date-and-time.vue
@@ -1,47 +1,60 @@
 <template lang="pug">
 //- Example.
+example(title="Today's current time" anchor="today-current-time")
+  template(#desc)
+    p.
+      When you choose to show the time in vue-cal, the current time of today's date will
+      be marked with a line (scroll to the current time to see it).#[br]
+      The line position will be updated every time the calendar current view is re-rendered (by interacting).#[br]
+      You can easily customize the now-line as you wish via CSS.
+      Changing the line and arrow color is as easy as:
+    ssh-pre.mt6(language="css" label="CSS" :dark="store.darkMode") .vuecal__now-line {border-color: #06c;}
+    p.mt4.
+      If you don't want this feature you can simply hide it: #[span.code .vuecal__now-line {display: none}].#[br]
+      This feature has no impact on performance.
+    p.
+      If you want the now line to keep accurate position even while your calendar is idle, you can use the option
+      #[span.code watchRealTime] (see more in the #[a(href="#api") API] section).
+  template(#code-html).
+    &lt;vue-cal xs view="day" :disable-views="['years', 'year', 'month']" /&gt;
+
+  vue-cal(
+    :time-cell-height="26"
+    view="day"
+    :views="['day']"
+    :views-bar="false"
+    :today-button="false"
+    @ready="$event.view && $event.view.scrollToCurrentTime"
+    :dark="store.darkMode"
+    xs)
+
+//- Example.
 example(title="Scroll the View to a Particular Time" anchor="scroll-to-time")
   template(#desc)
     p.mb0.
       It is quite easy to scroll the calendar view to a particular time from outside of Vue Cal.
-    w-button.mt2.mr2(@click="exScrollToTime.scrollToCurrentTime('.ex--scroll-to-time')")
+    w-button.mt2.mr2(@click="exScrollToTime.scrollToCurrentTime")
       w-icon mdi mdi-format-vertical-align-bottom
       | Scroll to current time
-    w-button.mt2.mr2(@click="exScrollToTime.scrollToTop('.ex--scroll-to-time')")
+    w-button.mt2.mr2(@click="exScrollToTime.scrollTop")
       w-icon mdi mdi-format-vertical-align-top
       | Scroll top
+    w-button.mt2.mr2(@click="exScrollToTime.scrollToTime(12 * 60)")
+      w-icon mdi mdi-format-vertical-align-center
+      | Scroll to 12:00
   template(#desc2)
-    .w-flex.gap2.wrap
-      vue-cal.ex--scroll-to-time(
-        :dark="store.darkMode"
-        xs
-        view="day"
-        :disable-views="['years', 'year', 'month', 'week']"
-        :views-bar="false"
-        :time-cell-height="exScrollToTime.timeCellHeight"
-        @ready="exScrollToTime.scrollToCurrentTime('.ex--scroll-to-time')")
-      div
-        ssh-pre(language="html-vue" :dark="store.darkMode").
-          &lt;vue-cal
-            ref="calendarEl"
-            :time-cell-height="timeCellHeight"
-            @ready="scrollToCurrentTime"&gt;
-          &lt;/vue-cal&gt;
-        ssh-pre(language="js" :dark="store.darkMode").
-          const timeCellHeight = 26
-          const calendarEl = ref(null)
-
-          function scrollToCurrentTime () {
-            const now = new Date()
-            const calendar = calendarEl.value.querySelector('.vuecal__scrollable')
-            const hours = now.getHours() + now.getMinutes() / 60
-            calendar.scrollTo({ top: hours * timeCellHeight, behavior: 'smooth' })
-          }
-
-          function scrollToTop () {
-            const calendar = calendarEl.value.querySelector('.vuecal__scrollable')
-            calendar.scrollTo({ top: 0, behavior: 'smooth' })
-          }
+    vue-cal.ex--scroll-to-time(
+      :dark="store.darkMode"
+      view="day"
+      :views="['day']"
+      :views-bar="false"
+      :today-button="false"
+      :time-cell-height="exScrollToTime.timeCellHeight"
+      @ready="exScrollToTime.onReady"
+      xs)
+    ssh-pre(language="html-vue" :dark="store.darkMode").
+      &lt;vue-cal @ready="scrollToCurrentTime"&gt;
+      &lt;/vue-cal&gt;
 
 //- Example.
 example(title="Timeline Tweaking" anchor="timeline-tweaking")
@@ -53,11 +66,9 @@ example(title="Timeline Tweaking" anchor="timeline-tweaking")
       So if you don't set this class you are free to paint the lines yourself or not.
   template(#code-html).
     &lt;vue-cal
-      small
       :time-from="5 * 60"
       :time-step="15"
       :time-cell-height="18"
-      view="day"
       :disable-views="['years', 'year', 'month']"
       hide-weekends&gt;
       &lt;template #time-cell="{ hours, minutes }"&gt;
@@ -75,76 +86,20 @@ example(title="Timeline Tweaking" anchor="timeline-tweaking")
       #[a(href="https://vuejs.org/guide/components/slots.html#scoped-slots" target="_blank") vuejs.org/guide/components/slots.htm #[w-icon(color="primary") mdi mdi-open-in-new]]
 
   vue-cal(
-    style="width: 360px;height: 360px;max-width: 100%"
     :dark="store.darkMode"
-    small
+    sm
     :time-from="5 * 60"
     :time-step="15"
     :time-cell-height="18"
     view="day"
     :disable-views="['years', 'year', 'month']"
+    style="width: 360px;height: 360px;max-width: 100%"
     hide-weekends)
     template(#time-cell="{ hours, minutes }")
       .vuecal__time-cell-line(:class="{ hours: !minutes }")
         strong.primary(v-if="!minutes" style="font-size: 15px;line-height: 18px") {{ hours }}
         span(v-else style="font-size: 11px;line-height: 18px") {{ minutes }}
 
-//- Example.
-example(title="Today's current time" anchor="today-current-time")
-  template(#desc)
-    p.
-      When you choose to show the time in vue-cal, the current time of today's date will
-      be marked with a line (scroll to the current time to see it).#[br]
-      The line position will be updated every time the calendar current view is re-rendered (by interacting).#[br]
-      You can easily customize the now-line as you wish via CSS.
-      Changing the line and arrow color is as easy as:
-    ssh-pre.mt6(language="css" label="CSS" :dark="store.darkMode") .vuecal__now-line {color: #06c;}
-    p.mt4.
-      If you don't want this feature you can simply hide it: #[span.code .vuecal__now-line {display: none}].#[br]
-      This feature has no impact on performance.
-    p.
-      If you want the now line to keep accurate position even while your calendar is idle, you can use the option
-      #[span.code watchRealTime] (see more in the #[a(href="#api") API] section).
-  template(#code-html).
-    &lt;vue-cal xs active-view="day" :disable-views="['years', 'year', 'month']" /&gt;
-
-  vue-cal.ex--today-current-time(
-    :dark="store.darkMode"
-    xs
-    :time-cell-height="26"
-    view="day"
-    :disable-views="['years', 'year', 'month']"
-    @ready="exScrollToTime.scrollToCurrentTime('.ex--today-current-time')"
-    style="width: 360px;height: 360px;max-width: 100%")
-
-
-//- Example.
-//- example(title="" anchor="")
-  template(#desc)
-  template(#code-html).
-    &lt;vue-cal{{}} /&gt;
-  vue-cal()
-
-//- Example.
-//- example(title="" anchor="")
-  template(#desc)
-  template(#code-html).
-    &lt;vue-cal{{}} /&gt;
-  vue-cal()
-
-//- Example.
-//- example(title="" anchor="")
-  template(#desc)
-  template(#code-html).
-    &lt;vue-cal{{}} /&gt;
-  vue-cal()
-
-//- Example.
-//- example(title="" anchor="")
-  template(#desc)
-  template(#code-html).
-    &lt;vue-cal{{}} /&gt;
-  vue-cal()
 
 //- Example.
 //- example(title="" anchor="")
@@ -190,18 +145,24 @@ const vuecalEl = ref(null)
 
 const exScrollToTime = reactive({
   timeCellHeight: 26,
-  scrollToCurrentTime: vuecal => {
-    const now = new Date()
-    const calendar = document.querySelector(`${vuecal} .vuecal__scrollable`)
-    const hours = now.getHours() + now.getMinutes() / 60
-    calendar.scrollTo({ top: hours * exScrollToTime.timeCellHeight, behavior: 'smooth' })
-  },
-  scrollToTop: vuecal => {
-    const calendar = document.querySelector(`${vuecal} .vuecal__scrollable`)
-    calendar.scrollTo({ top: 0, behavior: 'smooth' })
+  scrollTop: null,
+  scrollToTime: null,
+  scrollToCurrentTime: null,
+  onReady: ({ view }) => {
+    exScrollToTime.scrollTop = view.scrollTop
+    exScrollToTime.scrollToTime = view.scrollToTime
+    exScrollToTime.scrollToCurrentTime = view.scrollToCurrentTime
+    view.scrollToCurrentTime()
   }
 })
 </script>
+
+<style lang="scss">
+// Today-current-time example.
+.example--today-current-time {
+  .vuecal__now-line {border-color: #06c;}
+}
+</style>
 
 <style lang="scss" scoped>
 .example .vuecal:not(.vuecal--date-picker) {height: 300px;}
