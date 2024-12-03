3a2c4d4f3be0ab73dca9875db40c22abdf3f2d98
antoniandre <antoniandre.web@gmail.com>
2024-12-03 10:58:35 +0100

    Doc: Remove the examples source code expandable button when too short.

--- src/documentation/components/example.vue
+++ src/documentation/components/example.vue
@@ -5,7 +5,7 @@
   .example__desc(v-if="$slots.desc")
     slot(name="desc")
 
-  .source-wrap.w-flex.column.gap3.mt3(:class="{ expanded }")
+  .source-wrap.w-flex.column.gap3.mt3(ref="sourceWrapEl" :class="{ expanded, 'no-scroll': sourceWrapNoScroll }")
     ssh-pre.example__source(
       v-if="$slots['code-html']"
       language="html-vue"
@@ -24,7 +24,7 @@
       data-label="CSS"
       :dark="store.darkMode")
       slot(name="code-css")
-    w-button.example__source-expand(sm round @click="expanded = !expanded")
+    w-button.example__source-expand(v-if="!sourceWrapNoScroll" sm round @click="expanded = !expanded")
       w-icon.mr1 mdi mdi-unfold-{{ expanded ? 'less' : 'more' }}-horizontal
       | {{ expanded ? 'Collapse' : 'Expand' }} Source Code
 
@@ -36,7 +36,7 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { nextTick, onMounted, ref } from 'vue'
 import { useAppStore } from '@/store'
 
 const store = useAppStore()
@@ -47,6 +47,18 @@ defineProps({
 })
 
 const expanded = ref(false)
+const sourceWrapEl = ref(null)
+const sourceWrapNoScroll = ref(false)
+
+onMounted(async () => {
+  await nextTick()
+  await new Promise(resolve => setTimeout(resolve, 1000))
+
+  console.log(sourceWrapEl.value.scrollHeight, sourceWrapEl.value.clientHeight)
+  if (sourceWrapEl.value.scrollHeight <= sourceWrapEl.value.clientHeight) {
+    sourceWrapNoScroll.value = true
+  }
+})
 </script>
 
 <style lang="scss">
@@ -54,7 +66,7 @@ const expanded = ref(false)
   .source-wrap {
     position: relative;
     overflow-y: hidden;
-    max-height: 300px;
+    max-height: 250px;
     border: 1px solid color-mix(in srgb, var(--w-contrast-bg-color) 10%, transparent);
     border-radius: 6px;
     transition: all 0.35s ease-in-out, overflow 0s 0.4s;
@@ -77,7 +89,7 @@ const expanded = ref(false)
       pointer-events: none;
       transition: 0.35s ease-in-out;
     }
-    &.expanded:after {opacity: 0;}
+    &.expanded:after, &.no-scroll:after {opacity: 0;}
   }
 
   .w-app &__source-expand {
@@ -97,5 +109,6 @@ const expanded = ref(false)
     border-bottom: none;
     padding-bottom: 24px; // Leave some space for the expand button.
   }
+  .w-app .source-wrap.no-scroll &__source[data-label]:last-of-type {padding-bottom: 8px;}
 }
 </style>
