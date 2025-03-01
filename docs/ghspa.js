9863017daceebac775fd63f28b034ee7b68e6eb1
antoniandre <antoniandre.web@gmail.com>
2025-03-01 01:26:43 +0100

    Publish documentation on Github.

--- docs/ghspa.js
+++ docs/ghspa.js
@@ -1,42 +1,44 @@
 /**
- *  Fix for Single Page Applications (SPA) on GitHub Pages
+ *  Easy way to enable Single Page Applications for GitHub Pages
+ *
+ *  This project was released under MIT license.
  *
  *  @link      https://github.com/rafrex/spa-github-pages
- *  @author    Rafael Pedicini
- *  @fixes     Supports deeper nested routes properly
+ *  @author    Rafael Pedicini <code@rafrex.com>
+ *  @link      http://websemantics.ca
+ *  @author    Adnan M.Sagar, PhD. <adnan@websemantics.ca>
+ *
+ *  @param {Object} l, the document current location
  */
-
 ;(function(l) {
-  // Automatically detect full repository name from GitHub Pages path
-  var pathParts = l.pathname.split('/').filter(Boolean)
-  var repo = '/' + pathParts[0] // Always set this to the repo name
+  var repo = '/' + l.pathname.split('/')[1]
 
-  /* Redirect all 404 traffic to index.html */
-  function redirect() {
-    l.replace(l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') + repo + '/?' +
-      (l.pathname ? 'p=' + l.pathname.replace(/&/g, '~and~').replace(repo, '') : '') +
-      (l.search ? '&q=' + l.search.slice(1).replace(/&/g, '~and~') : '') +
-      (l.hash || '')
-    )
-  }
+   /* redirect all 404 trafic to index.html */
+   function redirect() {
+     l.replace(l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') + repo + '/?' +
+              (l.pathname ? 'p=' + l.pathname.replace(/&/g, '~and~').replace(repo, '') : '') +
+              (l.search ? '&q=' + l.search.slice(1).replace(/&/g, '~and~') : '') +
+              (l.hash))
+   }
 
-  /* Resolve 404 redirects into internal routes */
-  function resolve() {
-    if (l.search) {
-      var q = {}
-      l.search.slice(1).split('&').forEach(function(v) {
-        var a = v.split('=')
-        q[a[0]] = a.slice(1).join('=').replace(/~and~/g, '&')
-      })
+   /* resolve 404 redirects into internal routes */
+   function resolve() {
+     if (l.search) {
+       var q = {}
+       l.search.slice(1).split('&').forEach(function(v) {
+         var a = v.split('=')
+         q[a[0]] = a.slice(1).join('=').replace(/~and~/g, '&')
+       })
+       if (q.p !== undefined) {
+         window.history.replaceState(null, null,
+           repo + (q.p || '') +
+           (q.q ? ('?' + q.q) : '') +
+           l.hash
+         )
+       }
+     }
+   }
 
-      if (q.p !== undefined) {
-        var newPath = repo + (q.p || '') + (q.q ? ('?' + q.q) : '') + (l.hash || '')
-        window.history.replaceState(null, null, newPath)
-      }
-    }
-  }
-
-  /* If current document is the 404 page, redirect to index.html, otherwise resolve */
-  if (document.title === '404') redirect()
-  else resolve()
+  /* if current document is 404 page page, redirect to index.html otherwise resolve */
+  document.title === '404' ? redirect() : resolve()
 }(window.location))
