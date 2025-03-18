259e3998e32d7fff141ee42295bc6e0f7ab287c1
Jonas Stahl <Jonas.M.Stahl@gmail.com>
2025-03-18 23:33:29 +0100

    Add types for emits

--- src/types/vue-cal.d.ts
+++ src/types/vue-cal.d.ts
@@ -1,26 +1,29 @@
-import {DefineComponent} from 'vue';
+import {
+  Ref,
+  DefineSetupFnComponent
+} from 'vue';
 
-type DateString = `${number}${number}${number}${number}-${number}${number}-${number}${number}`
-type DateTimeString = `${DateString} ${number}${number}:${number}${number}`
+type VueCalDateString = `${number}${number}${number}${number}-${number}${number}-${number}${number}`
+type VueCalDateTimeString = `${VueCalDateString} ${number}${number}:${number}${number}`
 
-type Weekdays = 'mon' | 'tue' | 'wed' | 'thu' | 'fri' | 'sat' | 'sun'
-type Languages = 'ar' | 'bg' | 'bn' | 'bs' | 'ca' | 'cs' | 'da' | 'de' | 'el' | 'en-gb' | 'en-us' | 'es' | 'et' | 'fa' | 'fi' | 'fr' | 'he' | 'hr' | 'hu' | 'id' | 'is' | 'it' | 'ja' | 'ka' | 'ko' | 'lt' | 'mn' | 'nl' | 'no' | 'pl' | 'pt-br' | 'pt-pt' | 'ro' | 'ru' | 'sk' | 'sl' | 'sq' | 'sr' | 'sv' | 'tr' | 'uk' | 'vi' | 'zh-cn' | 'zh-hk'
+type VueCalWeekdays = 'mon' | 'tue' | 'wed' | 'thu' | 'fri' | 'sat' | 'sun'
+type VueCalLanguages = 'ar' | 'bg' | 'bn' | 'bs' | 'ca' | 'cs' | 'da' | 'de' | 'el' | 'en-gb' | 'en-us' | 'es' | 'et' | 'fa' | 'fi' | 'fr' | 'he' | 'hr' | 'hu' | 'id' | 'is' | 'it' | 'ja' | 'ka' | 'ko' | 'lt' | 'mn' | 'nl' | 'no' | 'pl' | 'pt-br' | 'pt-pt' | 'ro' | 'ru' | 'sk' | 'sl' | 'sq' | 'sr' | 'sv' | 'tr' | 'uk' | 'vi' | 'zh-cn' | 'zh-hk'
 
-type Views = 'day' | 'days' | 'week' | 'month' | 'year' | 'years'
-type ViewsLayout = Partial<Record<Views, {
+type VueCalViewKeys = 'day' | 'days' | 'week' | 'month' | 'year' | 'years'
+type VueCalViewsLayout = Partial<Record<VueCalViewKeys, {
     cols: number
     rows: number
 }>>
 
-type SpecialConfigs = {
+export interface VueCalSpecialHoursConfigs {
     from: number
     to: number
     class: string
     label?: string
 }
-type SpecialHours = Partial<Record<WeekDays, SpecialConfigs | SpecialConfigs[]>>
+type VueCalSpecialHours = Partial<Record<VueCalWeekdays, VueCalSpecialHoursConfigs | VueCalSpecialHoursConfigs[]>>
 
-type Event = {
+export interface VueCalEvent {
     start: Date,
     end: Date,
     id?: string,
@@ -29,18 +32,18 @@ type Event = {
     resizable?: boolean,
     deletable?: boolean,
     allDay?: boolean,
-    recurring?: number,
+    // recurring?: number,
     schedule?: number,
     background?: number,
     class?: string
 }
 
-export interface VueCalProps {
+export interface VueCalConfig {
   // HANDLED:
   clickToNavigate?: boolean, // Setting to false will force it off on date-picker.
   dark?: boolean, // Dark theme.
   datePicker?: boolean, // Shorthand for xs: true, views: [month, year, years], clickToNavigate: true.
-  disableDays?: (Date | DateString)[], // Array of specific dates to disable.
+  disableDays?: (Date | VueCalDateString)[], // Array of specific dates to disable.
   // // Can be true false or a finer grain permissions object like:
   // // { drag: bool, resize: bool, create: bool, delete: bool }
   editableEvents?: boolean | { drag: boolean, resize: boolean, create: boolean, delete: boolean },
@@ -48,20 +51,20 @@ export interface VueCalProps {
   eventCreateMinDrag?: number, // The minimum drag distance in pixels to create an event.
   // The array of events to display in Vue Cal.
   // Can hold just the view events and be updated or the full array of all events available.
-  events?: Event[],
+  events?: VueCalEvent[],
   eventCount?: boolean, // Displays an events counter in each cell on month view.
   eventsOnMonthView?: boolean, // Displays events in full on month view.
-  hideWeekdays?: Weekdays[], // An array of strings. Possible values: 'mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'.
+  hideWeekdays?: VueCalWeekdays[], // An array of strings. Possible values: 'mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'.
   hideWeekends?: boolean, // Show or hide both Saturday and Sunday in days, week and month views.
   // en-us is the default and fallback if locale is not supported.
   // The locale can also be provided externally to avoid using Promises.
-  locale?: Languages, // A language to use for all the texts.
-  maxDate?: Date | DateString | DateTimeString, // Mostly for date pickers, sets a maximum date for cell interactions.
-  minDate?: Date | DateString | DateTimeString, // Mostly for date pickers, sets a minimum date for cell interactions.
+  locale?: VueCalLanguages, // A language to use for all the texts.
+  maxDate?: Date | VueCalDateString | VueCalDateTimeString, // Mostly for date pickers, sets a maximum date for cell interactions.
+  minDate?: Date | VueCalDateString | VueCalDateTimeString, // Mostly for date pickers, sets a minimum date for cell interactions.
   // A 2-way binding that highlights the selected date in the calendar but does not navigate to it.
-  selectedDate?: (Date | DateString),
+  selectedDate?: (Date | VueCalDateString),
   sm?: boolean, // Small size (truncates texts + specific styles).
-  specialHours?: SpecialHours, // Highlight a particular time range on each day of the week, individually.
+  specialHours?: VueCalSpecialHours, // Highlight a particular time range on each day of the week, individually.
   // schedules: { type: Array, default: () => [] }, // Split a day in different persons/rooms/locations schedules.
   snapToInterval?: number, // Snap the event start and end to a specific interval in minutes.
   startWeekOnSunday?: boolean, // Shows Sunday before Monday in days, week and month views.
@@ -75,16 +78,15 @@ export interface VueCalProps {
   timeTo?: number, // End time of the time column, in minutes.
   titleBar?: boolean, // Show or hide the header title bar.
   todayButton?: boolean, // Show or hide the header today button.
-  transitions?: boolean, // Enables/disables the navigation transitions.
   twelveHour?: boolean, // 12 or 24 hour format are respectively written like 1pm and 13:00.
   // Sets the calendar view to one of: 'day', 'days', 'week', 'month', 'year', 'years'. Default 'week' or 'month' if datePicker.
   // Gets updated on view navigation.
-  view?: Views | ViewsLayout,
-  viewDate?: Date | DateString, // The view will automatically set its start and end to present this date.
+  view?: VueCalViewKeys | VueCalViewsLayout,
+  viewDate?: Date | VueCalDateString, // The view will automatically set its start and end to present this date.
   // Only available for month and day views, this will shift the start of the view (left or right) by x days (signed integer).
   viewDayOffset?: number,
   // The list of all the view that will be available in this calendar.
-  views?: Views[] | ViewsLayout,
+  views?: VueCalViewKeys[] | VueCalViewsLayout,
   viewsBar?: boolean, // Show or hide the headers view selection bar.
   watchRealTime?: false, // More expensive, so only trigger on demand.
   weekNumbers?: boolean, // Show the weeks numbers in a column on month view.
@@ -96,12 +98,109 @@ export interface VueCalProps {
   allDayEvents?: { type: [Boolean, String], default: false }
 }
 
+export interface VueCalView {
+  // ID, Title
+  id: VueCalViewKeys
+  title: string
+  // Ranges
+  start: Date
+  end: Date
+  extendedStart: Date
+  extendedEnd: Date
+  // Cell dates
+  cellDates: {
+      start: Date,
+      end: Date,
+      startFormatted: VueCalDateString
+  }[],
+  // Events
+  events: Record<VueCalDateString, Ref<VueCalEvent>[]>
+  // Methods
+  switch: (view: VueCalViewKeys) => void
+  broader: () => void
+  narrower: () => void
+  previous: () => void
+  next: () => void
+  goToToday: () => void
+  updateViewDate: (date: Date) => void
+  updateSelectedDate: (date: Date) => void
+  createEvent: (event: VueCalEvent) => void
+  deleteEvent: (eventId: number | object, forceStage?: number) => void
+  scrollToCurrentTime: () => void
+  scrollToTime: (minutes: number) => void
+  scrollTop: () => void
+  // Others
+  viewDate: Date
+  selectedDate: Date
+  now: Date
+  containsToday: boolean
+  cols: number
+  rows: number
+  isDay: boolean
+  isDays: boolean
+  isWeek: boolean
+  isMonth: boolean
+  isYear: boolean
+  isYears: boolean
+}
+
+export interface VueCalCell {
+  broader: VueCalViewKeys
+  narrower: VueCalViewsKeys
+  goBroader: () => void
+  goNarrower: () => void
+  start: Date
+  end: Date
+}
+
+export interface VueCalCellEvents {
+  cell: VueCalCell
+  cursor: {
+    x: number
+    y: number
+    date: Date
+  }
+  e: PointerEvent
+}
+
+export interface VueCalEventEvents {
+  event: VueCalEvent
+  e: PointerEvent
+}
+
 export interface VueCalSlots {
 
 }
 
-export interface VueCalEmits {
+export interface VueCalEmits extends Record<string, ((...args: any[]) => any)> {
+  // Core Events
+  'ready': (value: { config: VueCalConfig, view: VueCalView }) => any
+  'viewChange': (value: VueCalView) => any // possible change to custom view
+  'update:view': (value: VueCalViewKeys) => any
+  'update:selectedDate': (value: Date) => any
+  'update:viewDate': (value: Date) => any
+  'update:events': (value: VueCalEvent[]) => any
+  // Cell-related Events
+  'cell-click': (value: VueCalCellEvents) => any
+  'cell-drag-start': (value: VueCalCellEvents) => any
+  'cell-drag': (value: VueCalCellEvents) => any
+  'cell-drag-end': (value: VueCalCellEvents) => any
+  'cell-hold': (value: VueCalCellEvents) => any
+  // Event-related Events
+  'event-hold': (value: VueCalEventEvents) => any
+  'event-create': (value: VueCalEventEvents & VueCalCellEvents) => any
+  'event-created': (value: VueCalEventEvents) => any
+  'event-delete': (value: VueCalEvent) => any
+  'event-dropped': (value: {originalEvent: VueCalEvent, event: VueCalEvent}) => any
+  'event-drag-start': (value: VueCalEventEvents) => any
+  'event-drag': (value: VueCalEventEvents) => any
+  'event-drag-end': (value: VueCalEventEvents) => any
+  'event-resize-start': (value: VueCalEventEvents) => any
+  'event-resize': (value: { _eid: number, end: Date, endTimeMinutes }) => any
+  'event-resize-end': (value: VueCalEventEvents) => any
 
+  // 'event-change': []
+  // 'event-drag-start', 'event-drag-end', 'event-resize-start', 'event-resize-end'
 }
 
-export declare const VueCal: DefineComponent<VueCalProps, VueCalSlots, VueCalEmits>
+export declare const VueCal: DefineSetupFnComponent<VueCalConfig, VueCalEmits>
