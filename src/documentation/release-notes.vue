d3598f4dd7517942d56a8a648447446f3fcc9590
antoniandre <antoniandre.web@gmail.com>
2025-03-20 10:10:10 +0100

    Update the release notes.

--- src/documentation/release-notes.vue
+++ src/documentation/release-notes.vue
@@ -3,6 +3,10 @@ h1.title1 Release Notes
 
 ul.history.ml2
   li.patch
+    strong.version 5.0.1-rc.12
+    ul
+      li Fix a Nuxt issue related to raw JSON import. #[issue-link 35]
+  li.patch
     strong.version 5.0.1-rc.11
     ul
       li Bring events to front on hover, but under the headings bar. #[issue-link 37]
