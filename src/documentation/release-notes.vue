dd962351de85396fd7a3b7f672216e01abe91559
antoniandre <antoniandre.web@gmail.com>
2026-05-03 23:02:35 +0700

    Update the release notes.

--- src/documentation/release-notes.vue
+++ src/documentation/release-notes.vue
@@ -7,6 +7,8 @@ ul.history.ml2
     ul
       li Added a current time label in the time column. #[issue-link 115]
       li Schedules: support non-numeric schedule ids and #[code schedule: 0] in overlap checks, cell interaction, and drag-and-drop (no #[code ~~] coercion on ids). #[issue-link 72] #[issue-link 75]
+      li Clear event stacking styles if #[code time] is false.
+      li Week view: fix forward navigation when #[code hideWeekends] and #[code startWeekOnSunday] are both enabled (next arrow could appear stuck). #[issue-link 67]
   li.patch
     strong.version 5.0.1-rc.40
     ul
