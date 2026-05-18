29f104060c5f7768cbecc6ea08de8a62ce1b1bfc
antoniandre <antoniandre.web@gmail.com>
2026-05-18 12:13:32 +0700

    Update the roadmap.

--- src/documentation/road-map.vue
+++ src/documentation/road-map.vue
@@ -15,6 +15,7 @@ ul.kanban
   li.kanban__column.kanban__column--doing
     .title 🚧 Doing
     .w-flex.column.gap1.pa1
+      .task Support timezones
       .task Resize Multiple day events on touch devices
 
   li.kanban__column.kanban__column--done.xs-hide
@@ -99,7 +100,6 @@ ul.kanban
   li.kanban__column.kanban__column--later.smd-hide
     .title 🕔 Later
     .w-flex.column.gap1.pa1
-      .task Support timezones
       .task Drag &amp; drop multiple day events
       .task Improve multiple day events Month view
 </template>
